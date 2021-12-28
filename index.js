bf719ef6a58f8bf12f28f9e9240d1c3cf8706f14
NezukoChans <87556399+NezukoChans28@users.noreply.github.com>
2021-12-28 14:15:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ require("./passport.js")(
 )
 const connect = async () => {
   await mongoose
-    .connect("mongodb://localhost/sfuxz", { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect("mongodb://localhost:27017/sfuxzbot", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("connected,,"))
     .catch((err) => console.log(err));
 };
